225f5649d315e45139c30dc638925e5f125c900b
Andrey Malevich <kennyhorror@gmail.com>
2012-10-09 02:40:51 +0300

    Some grammar for regular verbs

--- create.py
+++ create.py
@@ -5,7 +5,9 @@ import os
 import codecs
 
 N_rules = ['+N+Sg:0', '+N+Pl:^s']
-V_rules = ['+V:0']
+V_rules = ['+V:0',
+    '+V+Pr+Sg+1:^o',    '+V+Pr+Sg+2:^i',   '+V+Pr+Sg+3:^e',
+    '+V+Pr+Pl+1:^iamo', '+V+Pr+Pl+2:^te', '+V+Pr+Pl+3:^ono']
 A_rules = ['+A:0']
 
 output = codecs.open('italian.lexc', encoding='utf-8', mode='w+')
@@ -13,13 +15,44 @@ output = codecs.open('italian.lexc', encoding='utf-8', mode='w+')
 def print_header():
   output.write("""!!!italian.lexc!!!
 
-Multichar_Symbols +N +V +A +Sg +Pl
+Multichar_Symbols +N +V +A +Sg +Pl +1 +2 +3 +Pr +Pa
 
 LEXICON Root
 
 Noun ;
 Verb ;
 Adv  ;
+
+""")
+
+def print_foma():
+  output = codecs.open('italian.foma', encoding='utf-8', mode='w+')
+  output.write("""### italian.foma ###
+
+# Rules for writing verbs
+define VerbPrSg12Pl1 [a r e | e r e | i r e] -> 0 || _ "^" [ o | i | i a m o ];
+define VerbPrSg3First [ r e "^" e ] -> "^" || [ a ] _;
+define VerbPrSg3SecondThird [ e r e | i r e ] -> 0 || _ "^" [ e ];
+define VerbPrPl2 [ r e ] -> 0 || [a | e | i ] _ "^" [ t e ];
+define VerbPrPl3First [ a r e ] -> a || _ "^" [ n o ];
+define VerbPrPl3SecondThird [ e r e | i r e ] -> 0 || _ "^" [ o n o ];
+
+#Cleanup: remove morpheme boundaries
+define Cleanup "^" -> 0;
+
+read lexc italian.lexc
+define Lexicon
+
+define Grammar Lexicon              .o. 
+               VerbPrSg12Pl1        .o.
+               VerbPrSg3First       .o.
+               VerbPrSg3SecondThird .o.
+               VerbPrPl2            .o.
+               VerbPrPl3First       .o.
+               VerbPrPl3SecondThird .o.
+               Cleanup;
+
+regex Grammar;
 
 """)
 
@@ -65,3 +98,5 @@ if __name__ == '__main__':
   print_rules('Ninf', N_rules)
   print_rules('Vinf', V_rules)
   print_rules('Ainf', A_rules)
+
+  print_foma()
