b099c89cf88874e1e5e937ccb4d75d0f339fcbb9
Andrey Malevich <kennyhorror@gmail.com>
2012-10-18 23:22:11 +0300

    Fixed tester

--- run_test.py
+++ run_test.py
@@ -4,21 +4,26 @@ import codecs
 import subprocess
 
 if __name__ == '__main__':
-  words = []
+  words = [u'source italian.foma\nup\n']
+  questions = []
+  answers = []
   for line in open('italian.txt.learn', 'r'):
     columns = line.split()
+    questions.append(columns[0].decode("ISO-8859-2"))
     words.append(columns[0].decode("ISO-8859-2") + u'\n')
-  stdin = u'source italian.foma\nup\n'.join(words)
-
+    answers.append(u''.join(map(lambda x:x.decode("ISO-8859-2"), columns[1:])))
+  stdin = u''.join(words)
   process = subprocess.Popen("./foma", stdin = subprocess.PIPE,
       stdout = subprocess.PIPE, stderr = subprocess.PIPE)
   stdout, stderr = process.communicate(stdin.encode('utf-8'))
   output = codecs.open('italian.txt.result', encoding='utf-8', mode='w+')
-  itr = 0
-  for line in stdout.decode('utf-8').split('\n')[:-3]:
-    itr += 1
-    if itr < 27:
-      continue
-    output.write(line.split('> ')[-1])
+  for word, result, answer in zip(questions,
+                                  stdout.decode('utf-8').split('\n')[26:-3],
+                                  answers):
+    output.write(word)
+    output.write('\t')
+    output.write(result.split('> ')[-1])
+    output.write('\t')
+    output.write(answer)
     output.write('\n')
   output.close()
