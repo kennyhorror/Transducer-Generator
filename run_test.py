3dafbb224040a3c9a6275d208d310e5dabba3f14
yasek <mr.yaski@gmail.com>
2012-10-21 16:34:38 +0300

    small refactoring

--- run_test.py
+++ run_test.py
@@ -11,14 +11,13 @@ except:
     pass
 
 if __name__ == '__main__':
-    words = []
     questions = []
     answers = []
 
-    for line in open('italian.txt.learn', 'r'):
+    for line in codecs.open('italian.txt.learn', encoding='ISO-8859-1', mode='r'):
         columns = line.split()
-        questions.append(columns[0].decode("ISO-8859-1"))
-        answers.append(u''.join(x.decode("ISO-8859-1") for x in columns[1:]))
+        questions.append(columns[0])
+        answers.append(u''.join(columns[1:]))
                                              
     process = subprocess.Popen([FOMA_PATH + "flookup", "italian.bin", "-x"], 
                                stdin=subprocess.PIPE,
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     fomas = stdout.decode('utf-8').split('\n\n')
 
     with codecs.open('italian.txt.result', encoding='utf-8', mode='w+') as output:
-        for word, results, answer in zip(questions, fomas, answers):
+        for (word, results, answer) in zip(questions, fomas, answers):
             result = '\t'.join(results.split('\n'))
             output.write("%s\t%s\t%s\n" % (word, answer, result))
 
