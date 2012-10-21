ee1508b758ab4dc04090a537506b88d992a235e1
yasek <mr.yaski@gmail.com>
2012-10-21 17:10:46 +0300

    Almost PEP8

--- run_test.py
+++ run_test.py
@@ -3,7 +3,7 @@
 import codecs
 import subprocess
 
-FOMA_PATH = "./"
+FOMA_PATH = './'
 
 try:
     from configlocal import *
@@ -19,16 +19,17 @@ if __name__ == '__main__':
         questions.append(columns[0])
         answers.append(u''.join(columns[1:]))
                                              
-    process = subprocess.Popen([FOMA_PATH + "flookup", "italian.bin", "-x"], 
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen([FOMA_PATH + 'flookup', 'italian.bin', '-x'], 
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
     stdin = u'\n'.join(questions)
-    stdout, stderr = process.communicate(stdin.encode('utf-8'))
+    (stdout, stderr) = process.communicate(stdin.encode('utf-8'))
 
     fomas = stdout.decode('utf-8').split('\n\n')
 
     with codecs.open('italian.txt.result', encoding='utf-8', mode='w+') as output:
         for (word, results, answer) in zip(questions, fomas, answers):
             result = '\t'.join(results.split('\n'))
-            output.write("%s\t%s\t%s\n" % (word, answer, result))
+            output.write('%s\t%s\t%s\n' % (word, answer, result))
 
